a29c90df7734e01ac06943c11c59eb7370ef4e65
Serge Krykov <sergkryk@Serges-iMac.local>
2020-02-28 11:49:32 +0300

    добавляет простой js для модалки

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,38 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+var callRequestButton = document.querySelector('.site-header__button');
+var callRequestModal = document.querySelector('.call-request');
+var callRequestCloseBtn = callRequestModal.querySelector('.call-request__close-btn');
 
-pageHeader.classList.remove('page-header--nojs');
+var closeModal = function () {
+  callRequestModal.classList.add('call-request--hidden');
+};
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
+var onEscPressRemove = function (evt) {
+  if (evt.keyCode === 27) {
+    closeModal();
+    removeListeners();
   }
+};
+
+var onClickRemove = function (evt) {
+  if (evt.target === callRequestCloseBtn || evt.target === callRequestModal) {
+    closeModal();
+    removeListeners();
+  }
+};
+
+var addListeners = function () {
+  document.addEventListener('keypress', onEscPressRemove);
+  document.addEventListener('click', onClickRemove);
+};
+
+
+var removeListeners = function () {
+  document.removeEventListener('keypress', onEscPressRemove);
+  document.removeEventListener('click', onClickRemove);
+};
+
+callRequestButton.addEventListener('click', function () {
+  callRequestModal.classList.remove('call-request--hidden');
+  addListeners();
 });
