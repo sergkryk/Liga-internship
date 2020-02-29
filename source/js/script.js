ef19dbd685eb8603ee43a389f0425684f4e72b15
Serge Krykov <sergkryk@Serges-iMac.local>
2020-02-29 18:28:50 +0300

    добавляет валидацию данных

--- source/js/script.js
+++ source/js/script.js
@@ -1,10 +1,16 @@
 'use strict';
 var callRequestButton = document.querySelector('.site-header__button');
-var callRequestModal = document.querySelector('.call-request');
-var callRequestCloseBtn = callRequestModal.querySelector('.call-request__close-btn');
+var popup = document.querySelector('.call-request');
+var popupCloseBtn = popup.querySelector('.call-request__close-btn');
+var form = popup.querySelector('.call-request__form');
+var login = popup.querySelector('[name=user-name]');
+var phone = popup.querySelector('[name=user-phone]');
+var question = popup.querySelector('[name=user-question]');
+var loginStorage = localStorage.getItem('login');
+var phoneStorage = localStorage.getItem('phone');
 
 var closeModal = function () {
-  callRequestModal.classList.add('call-request--hidden');
+  popup.classList.add('call-request--hidden');
 };
 
 var onEscPressRemove = function (evt) {
@@ -15,14 +21,14 @@ var onEscPressRemove = function (evt) {
 };
 
 var onClickRemove = function (evt) {
-  if (evt.target === callRequestCloseBtn || evt.target === callRequestModal) {
+  if (evt.target === popupCloseBtn || evt.target === popup) {
     closeModal();
     removeListeners();
   }
 };
 
 var addListeners = function () {
-  document.addEventListener('keypress', onEscPressRemove);
+  document.addEventListener('keydown', onEscPressRemove);
   document.addEventListener('click', onClickRemove);
 };
 
@@ -32,7 +38,45 @@ var removeListeners = function () {
   document.removeEventListener('click', onClickRemove);
 };
 
-callRequestButton.addEventListener('click', function () {
-  callRequestModal.classList.remove('call-request--hidden');
+callRequestButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('call-request--hidden');
+  login.focus();
+
+  if (loginStorage) {
+    login.value = loginStorage;
+    phone.focus();
+  }
+
+  if (phoneStorage) {
+    phone.value = phoneStorage;
+    question.focus();
+  }
   addListeners();
+});
+
+form.addEventListener('submit', function () {
+  localStorage.setItem('login', login.value);
+  localStorage.setItem('phone', phone.value);
+});
+
+phone.addEventListener('focus', function () {
+  var oldValue = phone.value;
+  if (!oldValue) {
+    var countryCode = '+7 (';
+    phone.value = countryCode;
+  }
+});
+
+phone.addEventListener('input', function (e) {
+  if (e.data) {
+    var x = e.target.value.replace(/\D/g, '').match(/^(\d{0,1})(\d{0,3})(\d{0,3})(\d{0,2})(\d{0,2})/);
+    var countryCode = '+' + x[1] + ' ';
+    var operatorCode = x[2].length === 3 ? '(' + x[2] + ') ' : '(' + x[2];
+    var firstBlock = x[3].length === 3 ? x[3] + '-' : x[3];
+    var secondBlock = x[4].length === 2 ? x[4] + '-' : x[4];
+    var thirdBlock = x[5];
+    var fullNumber = !x[3] ? countryCode + operatorCode : countryCode + operatorCode + firstBlock + secondBlock + thirdBlock;
+    e.target.value = !x[2] ? countryCode + '(' : fullNumber;
+  }
 });
